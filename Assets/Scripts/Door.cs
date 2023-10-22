a0b7917b63c0667aec9517806c6c98937c4eced5
Andrew Hartnell <62224502+ThirdByteTaken@users.noreply.github.com>
2023-10-21 19:08:35 -0700

    Add door system

---
+++ Assets/Scripts/Door.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Door : MonoBehaviour
+{
+    [SerializeField]
+    private string Target_Scene;
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                SceneManager.LoadScene(Target_Scene, LoadSceneMode.Single);
+            }
+        }
+    }
+}
