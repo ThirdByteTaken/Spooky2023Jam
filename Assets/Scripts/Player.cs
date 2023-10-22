b592ff4adaf33f074e17a7143a6abd90dcc2d31e
Andrew Hartnell <62224502+ThirdByteTaken@users.noreply.github.com>
2023-10-21 18:51:51 -0700

    Add jump mechanic

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,8 +6,16 @@ public class Player : MonoBehaviour
 {
     [SerializeField]
     private float Movement_Speed;
+    [SerializeField]
+    private float Jump_Height;
+
+    [SerializeField]
+    private bool Can_Jump;
+
     private Rigidbody2D rb;
     private Camera mainCamera;
+
+    private bool isGrounded = true;
     
 
     private void Start()
@@ -23,9 +31,23 @@ public class Player : MonoBehaviour
 
         float xMovement = horizontalInput * Time.deltaTime * Movement_Speed;
 
+        if (Can_Jump && Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rb.AddForce(new Vector2(0, Jump_Height));
+            isGrounded = false;
+	    }
+
         Vector2 velocity = rb.velocity;
         velocity.x = xMovement * Movement_Speed;
         rb.velocity = velocity;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Floor"))
+        {
+            isGrounded = true;
+	    }
     }
 
     private void Update()
